1f80e92a0f1a9955c27695c5234f0119d361f15c
arefvandadi <aref.vandadi@gmail.com>
2024-12-01 09:29:23 -0500

    Update app.py to set the datatype to float32 and convert the numpy array to torch so that imported CNN model can work with that input, Add CNN model definition and temporarily imported a local model to test if the predict route work

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from PIL import Image
 from io import BytesIO
 import base64
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 app = Flask(__name__)
 
@@ -19,13 +22,44 @@ def predict():
     image_decoded = base64.b64decode(image_dataUrl)
     image = Image.open(BytesIO(image_decoded))
     image = image.convert('L') # ensure removing any unwanted channels from the browser which can be in RGB format.
-    image.save("drawing.png")
+    # image.save("drawing.png")
     # print(image.size)
     # print(type(image))
     image_array = np.array(image.resize((28, 28)))/255
-    image_array = image_array.reshape(1, 1, 28, 28)
+    image_array = image_array.reshape(1, 1, 28, 28).astype(np.float32)
+    image_array = torch.from_numpy(image_array)
     # print(image_array.shape)
     # print(image_array)
+    # print(image_array.dtype)
+
+
+    ######################### Model Definition:
+    class NeuralNet(nn.Module):
+        def __init__(self):
+            super(NeuralNet, self).__init__()
+            self.conv1 = nn.Conv2d(1, 10, kernel_size = 5)
+            self.conv2 = nn.Conv2d(10, 20, kernel_size = 5)
+            self.map = nn.MaxPool2d(2)
+            
+            self.fc = nn.Linear(320,10)
+        
+        def forward(self,x):
+            in_size = x.size(0)
+            x = F.relu(self.map(self.conv1(x)))
+            x = F.relu(self.map(self.conv2(x)))
+            x = x.view(in_size,-1) #flatten the tensor
+            
+            x = self.fc(x)
+            
+            return F.log_softmax(x)
+
+    model = NeuralNet()
+    model.load_state_dict(torch.load("./Notebooks/CNN_Model_state_dict.pt"))
+    model.eval()
+
+    label_pred = model(image_array)
+    _, class_pred = torch.max(label_pred,1)
+    print(class_pred.item())
     return "Test"
 
 
